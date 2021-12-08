fa8b1aa5ce6f624d57caf1400d1168fe2c938f6a
Rahul Bansal <b369rahul@git.com>
2021-12-08 20:12:27 +0530

    khatma

---
+++ home.js
@@ -0,0 +1,6 @@
+var abc = function (event) {
+    var name = document.querySelector("#fname").value + " " 
+    + document.querySelector("#lname").value ;
+    document.querySelector(".hiabc").innerHTML = name;
+};
+
